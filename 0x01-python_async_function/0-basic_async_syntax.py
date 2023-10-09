1f381d1ea9d67f02ff9d136d4bf9bd6478be2e1e
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-10-09 13:15:38 +0000

    Await the randomly genreated integer and then return it

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,6 +2,7 @@
 """module with function named wait_random"""
 
 
+import asyncio
 import random
 
 
@@ -9,4 +10,6 @@ async def wait_random(max_delay: int=10) -> int:
     """
     function that waits for a random delay between 0 and max_delay
     """
-    return random.uniform(0, max_delay)
+    num = random.uniform(0, max_delay)
+    await asyncio.sleep(num)
+    return num
