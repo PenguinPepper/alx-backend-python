fbe2b0db78e2107816dd550143a934810fd16577
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-10-11 13:33:49 +0000

    Change min_delay variable name to max_delay and add while loop to sort list in ascending order

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,13 +9,19 @@ from typing import List
 wait = importlib.import_module('0-basic_async_syntax')
 
 
-async def wait_n(n: int, min_delay: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Function that calls wait_random n times
     """
 
     new_list = []
     for i in range(n):
-        new_list.append(asyncio.create_task(wait.wait_random(min_delay)))
+        new_list.append(asyncio.create_task(wait.wait_random(max_delay)))
         completed, pending = await asyncio.wait(new_list)
-    return [task.result() for task in completed]
+    sort = [task.result() for task in completed]
+    sorted_list = []
+    while sort:
+        min_value = min(sort)
+        sorted_list.append(min_value)
+        sort.remove(min_value)
+    return sorted_list
