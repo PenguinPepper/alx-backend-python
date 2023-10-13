4ff5811de9a470d2f4a10e7842f005822cc550ed
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-10-13 14:47:40 +0000

    Edit file

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator:
-    """Function yeilds a andom number between 0 and 10"""
+    """Function yeilds a random number between 0 and 10"""
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
