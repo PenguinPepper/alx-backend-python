05b41fc38a0e166a43dbbf0ff50cf7e332f34a0c
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-10-13 14:05:50 +0000

    Edit file to meet PEP8 standards and return the correct type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,8 +3,10 @@
 
 import asyncio
 import random
+from typing import AsyncGenerator
 
-async def async_generator() -> float:
+
+async def async_generator() -> AsyncGenerator:
     """Function yeilds a andom number between 0 and 10"""
     for i in range(10):
         await asyncio.sleep(1)
