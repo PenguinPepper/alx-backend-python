a65d73baf49051857bd3536c7474bd164f2b23d9
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-10-13 14:26:27 +0000

    Add correct return type

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,10 +3,10 @@
 
 import asyncio
 import random
-from typing import Generator
+from typing import AsyncGenerator
 
 
-async def async_generator() -> Generator[float]:
+async def async_generator() -> AsyncGenerator[float]:
     """Function yeilds a andom number between 0 and 10"""
     for i in range(10):
         await asyncio.sleep(1)
