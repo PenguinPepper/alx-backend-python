2516c362dbf7009421d8ca979182ea1b49df5612
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-10-13 14:28:11 +0000

    Revert back to previous version

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -12,6 +12,6 @@ async def measure_runtime() -> float:
     """measures runtime of comprehension"""
     start_time = time.time()
     await asyncio.gather(com.async_comprehension(), com.async_comprehension(),
-                         com.async_comprehension(), com.async_comprehension())
+            com.async_comprehension(), com.async_comprehension())
     stop_time = time.time()
     return stop_time - start_time
