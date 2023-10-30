c9dc3a94f4cf7d21dc3a9d13983cd23a89cd0fbe
Penguin Peps <112114400+PenguinPepper@users.noreply.github.com>
2023-10-30 22:45:28 +0200

    Use assertRaises as a context manager

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -23,4 +23,5 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"))
         ])
     def test_access_nested_map_exception(self, nested_map, path):
-        self.assertRaises(KeyError, utils.access_nested_map(nested_map, path))
+        with self.assertRaises(KeyError):
+            utils.access_nested_map(nested_map, path)
