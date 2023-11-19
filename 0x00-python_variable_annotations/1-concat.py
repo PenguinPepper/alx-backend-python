3cb3ff84eec888c25d869e3d5297f9ad7c79c3fd
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-11-19 08:13:55 +0000

    Add file with concat function that returns a concatenated string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module contains concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Takes a string str1 and a string str2 as arguments
+
+    Return:
+        str3(string): str1 + str2
+    """
+    str3 = str1 + str2
+    return str3
