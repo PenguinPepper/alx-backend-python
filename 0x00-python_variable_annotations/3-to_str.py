9b5b733f177fceaef95a1baa698c1c6684ab3768
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-11-19 08:33:06 +0000

    Add file that takes a float and returns its string representation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module contains to_str function"""
+
+
+def to_str(n: float) -> str:
+    """"Function takes in a float and returns the string representation
+    """
+    return str(n)
