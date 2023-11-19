f65bd9ef55614e561d9dccc7e90bf38d42ba7179
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-11-19 08:24:53 +0000

    Add file with function named floor that returns the floor of a float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module contains function called floor"""
+import math
+
+
+def floor(n: float) -> int:
+    """Takes in a number and floors
+    """
+    return math.floor(n)
